1701c2e9fb926224332e54a88c2ad77b9939119c
JonLevine <jlevine@students.hackleyschool.org>
2014-10-22 22:50:33 -0400

    map proto

--- app.rb
+++ app.rb
@@ -10,7 +10,23 @@ require './models'
 require 'bundler/setup'
 require 'rack-flash'
 
-
-
 set :sessions, true
 use Rack::Flash, :sweep=>true
+
+# def current_user
+# 	if session[:user_id]
+# 		if @current_user=User.find(session[:user_id]).blank? then redirect '/sign-in' else @current_user=User.find(session[:user_id]) end
+# 	else
+# 		redirect '/sign-in'
+# 	end
+# end
+
+get '/' do
+	redirect '/home'
+end
+
+get '/home' do
+	erb :home
+	# current_user
+	# @profile=User.find(@current_user.id).profile
+end
